1abd4badfb6dc662fc1d39ae31d24d868ada9218
Greg Fodor <gfodor@gmail.com>
2022-02-21 17:02:21 -0800

    Add and fix up deps for NAPI

--- src/main/main.js
+++ src/main/main.js
@@ -2,18 +2,44 @@ import { app, BrowserWindow } from "electron";
 import { is } from "electron-util";
 import * as path from "path";
 import { format } from "url";
+import cst from "c-struct";
+import fs from "fs";
+import ioctl from "ioctl";
+const DEVICE_NAME = "/dev/video4";
+const V4L2_BUF_TYPE_VIDEO_OUTPUT = 2;
+const V4L2_PIX_FMT_BGRA444 = 0x32314147; // 'GA12'
+
+app.disableHardwareAcceleration();
 
 let win = null;
 
+cst.register(
+  "v4l2_fmt_pix",
+  new cst.Schema({
+    type: cst.type.uint32,
+    width: cst.type.uint32,
+    height: cst.type.uint32,
+    pixelformat: cst.type.uint32,
+    field: cst.type.uint32,
+    bytesperline: cst.type.uint32,
+    sizeimage: cst.type.uint32,
+    colorspace: cst.type.uint32,
+    priv: cst.type.uint32,
+    flags: cst.type.uint32,
+    ycbcr_or_hsv_enc: cst.type.uint32,
+    quantization: cst.type.uint32,
+    xfer_func: cst.type.uint32,
+  })
+);
+
 async function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 820,
-    minHeight: 600,
-    minWidth: 650,
+    width: 1024,
+    height: 768,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
       preload: path.join(__dirname, "preload.js"),
+      offscreen: true,
     },
     show: false,
   });
@@ -31,6 +57,25 @@ async function createWindow() {
       })
     );
   }
+
+  let cam = -1;
+
+  fs.open(DEVICE_NAME, "as+", 0o666, (err, fd) => {
+    cam = fd;
+
+    const pixfmt = cst.packSync("v4l2_fmt_pix", {
+      type: V4L2_BUF_TYPE_VIDEO_OUTPUT,
+      width: 1024,
+      height: 768,
+      pixelformat: V4L2_PIX_FMT_BGRA444,
+    });
+  });
+
+  win.webContents.on("paint", (event, dirty, image) => {
+    const buf = image.getBitmap();
+    console.log(buf);
+    fs.writeFileSync("ex.png", image.toPNG());
+  });
 
   win.on("closed", () => {
     win = null;
--- src/renderer/App.js
+++ src/renderer/App.js
@@ -3,20 +3,17 @@ import React, { useEffect, useState } from "react";
 export function App() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCount((c) => c + 1);
-    }, 1000);
+  //useEffect(() => {
+  //  const timer = setInterval(() => {
+  //    setCount((c) => c + 1);
+  //  }, 1000);
 
-    return () => clearInterval(timer);
-  }, []);
+  //  return () => clearInterval(timer);
+  //}, []);
 
-  return (
-    <div className="padding">
-      <h1>Count {count}</h1>
-
-      <div>Hello world!</div>
-      <div>Fast full page reload from esbuild</div>
-    </div>
-  );
+  return <div className="padding"></div>;
 }
+//<h1>Count {count}</h1>
+
+//<div>Hello world!</div>
+//<div>Fast full page reload from esbuild</div>
