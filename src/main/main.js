98893b4a66331aae21da3b99c808d35a4e94ee4c
Greg Fodor <gfodor@gmail.com>
2022-02-24 22:03:49 -0800

    Bump to 720p

--- src/main/main.js
+++ src/main/main.js
@@ -50,8 +50,8 @@ const v4l2_fmt_pix = Struct({
 
 async function createWindow() {
   win = new BrowserWindow({
-    width: 800 / (PLATFORM === "mac" ? 2 : 1),
-    height: 450 / (PLATFORM === "mac" ? 2 : 1),
+    width: 1280 / (PLATFORM === "mac" ? 2 : 1),
+    height: 720 / (PLATFORM === "mac" ? 2 : 1),
     webPreferences: {
       nodeIntegration: false,
       preload: path.join(__dirname, "preload.js"),
@@ -83,8 +83,8 @@ async function createWindow() {
 
       const pixfmt = new v4l2_fmt_pix({
         type: V4L2_BUF_TYPE_VIDEO_OUTPUT,
-        width: 800,
-        height: 450,
+        width: 1280,
+        height: 720,
         pixelformat: V4L2_PIX_FMT_YUV420,
       });
 
